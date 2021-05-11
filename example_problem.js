990ef7db31b9dab733fd53b8fa3a861ab4eecf3a
edougher <ejdougher@gmail.com>
2021-05-10 23:11:22 -0400

    added eample_problem.js

---
+++ example_problem.js
@@ -0,0 +1,21 @@
+/*
+-The Knapsack Problem
+You got a list of item, where every item has a value and a weight.
+You got a bag that holds a maximum weight of X. 
+Write a program that maximizes the value of the items you put in the 
+bag whilst ensuring that you do not exceed the max weight.
+*/
+
+
+items = [
+    {id: 'a', val: 10, w: 3},
+    {id: 'b', val: 6, w: 8},
+    {id: 'c', val: 3, w: 3}
+]
+maxWeight = 8
+
+// solution = ['a', 'c']
+
+items.map(i => {
+
+})
