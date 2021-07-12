ca47454b9a6cf286d2c6da07889253079a08464b
Aaron Dougher <ejdougher@gmail.com>
2021-07-11 23:27:27 -0500

    added kadanes

---
+++ arrays/kadanes.js
@@ -0,0 +1,7 @@
+/*
+ --- Kadane's Algorithm ---
+given an array of integers, return the maximum sum of a subarray of the array.
+A 'subarray' is contiguous, and can include just one integer, or all of them.
+
+    
+*/  
