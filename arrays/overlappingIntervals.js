dc2fe83739126b58cc9d5db6176a4cc00d641ff8
Aaron Dougher <ejdougher@gmail.com>
2021-07-28 22:53:57 -0500

    working on overlappingIntervals

---
+++ arrays/overlappingIntervals.js
@@ -0,0 +1,13 @@
+/*
+Given an array of intervals( and array on two integers with a beginning and ending in ascending order, ex: [[1,3],[4,9],[8,12]] merge any OVERLAPPING arrays then return all non-overlapping intervals ex: [[1,3],[4,12]])
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+
+*/
+
+
+
+let intervals = [[1,3],[2,6],[8,10],[15,18]]
