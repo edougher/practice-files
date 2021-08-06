3fb421e0ea9669f067cea029486f3289dea032c9
Aaron Dougher <ejdougher@gmail.com>
2021-08-05 23:59:30 -0500

    deleteSpecificNode.js added

--- arrays/overlappingIntervals.js
+++ arrays/overlappingIntervals.js
@@ -9,5 +9,17 @@ Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
 */
 
 
+const overlappingInt = (ints) => {
+    for (let i = 0; i < ints.length - 1; i++) {
+        let temp = []
+        if (ints[i][1] < ints[i + 1][0]) {
+            temp.push(ints[i][1], ints[i + 1][0])
+            
+        }
+        
+    }
+    console.log(ints);
+}
+let intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
 
-let intervals = [[1,3],[2,6],[8,10],[15,18]]
+overlappingInt(intervals)
---
+++ linked_list/deleteSpecificNode.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = null;
+  }
+
+  // insert first node
+
+  // insert last node
+
+  //insert at index
+
+  // get at index
+
+  // remove at index
+
+  // clear list
+
+  // print list data
+}
