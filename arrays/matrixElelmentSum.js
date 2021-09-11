830ffce6d656962dd56b78edf61b74aeb327f566
Aaron Dougher <ejdougher@gmail.com>
2021-09-10 23:49:30 -0500

    finished array/matrixElementSum.js finished

---
+++ arrays/matrixElelmentSum.js
@@ -0,0 +1,32 @@
+function matrixElementSum(matrix) {
+//keep track of total cost/ints of inner arrays
+let total = 0
+// if a certain index contains 0 then all the indexs below will be "haunted"
+let indexArr = {}
+
+for(let i = 0; i < matrix.length; i++){
+    
+    for (let j = 0; j < matrix[i].length; j++){
+        let val = matrix[i][j]
+        if (indexArr.hasOwnProperty(j)) {
+            continue;
+        } else {
+            if (val === 0) {
+                indexArr[j] = true
+            } else {
+                total += val
+            }
+            
+        }
+
+    }
+    
+}
+return total
+}
+let matrix = [[0,1,1,2], 
+ [0,5,0,0], 
+ [2,0,3,3]]
+matrixElementSum(matrix)
+
+//answer is nine
